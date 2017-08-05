38064094eb88412dd5ec2e83f64ef9b6f0022b00
kb9zzo <seths@schrockdatabasesolutions.com>
2017-08-05 00:57:30 -0400

    learning text output

--- LearningPython.py
+++ LearningPython.py
@@ -3,3 +3,15 @@
 #I want to verify that I can output text
 #I do this using the text "zero to hero"
 print('zero to hero')
+
+
+#custom format of text output
+#this doesn't print the single quote
+print('''First line
+second line''')
+
+
+#printing single or double quotes
+print("I can print the double quote (\") using the the backslash")
+
+
